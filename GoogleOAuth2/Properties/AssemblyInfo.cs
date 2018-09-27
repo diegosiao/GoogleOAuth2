15e28a63fcc4aaa5ffabc936d574b5381c183e63
Diego Morais <diegosiao@gmail.com>
2018-09-26 23:30:38 -0300

    Publishing version 1.0.3

--- GoogleOAuth2/Properties/AssemblyInfo.cs
+++ GoogleOAuth2/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GoogleOAuth2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Google APIs Access Token generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("diegosiao@gmail.com")]
 [assembly: AssemblyProduct("GoogleOAuth2")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
